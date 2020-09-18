fc9256d0b2f74020bf909a1a61a403c2e305e5df
cdsuddreth <cdsuddreth@gmail.com>
2020-09-18 19:55:37 -0400

    took port variable out of index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const server = require('./server/server')
 
-const PORT = process.env.port || 80
+const PORT = process.env.port
 
 server.listen(PORT, () => {
     console.log(`Server listening on port ${PORT}`)
