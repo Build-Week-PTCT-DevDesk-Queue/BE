887ccb365d0ef14672699a16d1061c738e533a05
cdsuddreth <cdsuddreth@gmail.com>
2020-09-18 19:38:08 -0400

    changed port fallback variable, hoping that fixes heroku issues

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const server = require('./server/server')
 
-const PORT = process.env.port || 5000
+const PORT = process.env.port || 80
 
 server.listen(PORT, () => {
     console.log(`Server listening on port ${PORT}`)
