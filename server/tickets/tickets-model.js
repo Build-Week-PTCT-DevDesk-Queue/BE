4ee3c1906407ced24c948dfa27f8c3a307686f4d
Darrell-Conklin <dmconklin84@gmail.com>
2020-09-17 21:43:28 -0500

    tickets endpoints

--- server/tickets/tickets-model.js
+++ server/tickets/tickets-model.js
@@ -1,22 +1,51 @@
 const db = require('../../data/config')
 
-async function getTickets() {
+function get() {
     return db('tickets')
 }
 
-async function createTicket(ticket) {
+async function create(ticket) {
     const [id] = await db('tickets').insert(ticket)
-    return getTicketById(id)
+    return getById(id)
 }
 
-async function getTicketById(id) {
+function getById(id) {
     return db('tickets')
         .where({ id })
 		.first()
 }
 
+async function addHelper(id, helper_id) {
+    if (id === 0) {
+        await db('tickets')
+            .update('helper_id', null)
+            .where('id', id)
+    } else {
+        await db('tickets')
+            .update('helper_id', helper_id)
+            .where('id', id)
+    }
+    return getById(id)
+}
+
+async function updateStatus(id, status) {
+    await db('tickets')
+        .update('status', status)
+        .where('id', id)
+    return getById(id)
+}
+
+function getBy(filter) {
+    return db('tickets')
+        .where(filter)
+}
+
 module.exports = {
-    getTickets,
-    createTicket,
-    getTicketById
+    get,
+    getBy,
+    create,
+    getById,
+    addHelper,
+    updateStatus,
+    
 }
--- server/tickets/tickets-router.js
+++ server/tickets/tickets-router.js
@@ -1,19 +1,65 @@
 const express = require('express')
 const router = express.Router()
-const tickets = require('./tickets-model')
+const Tickets = require('./tickets-model')
 
-router.post('/create', async (req,res,next) => {
+router.get('/', async (req,res,next) => {
     try {
-        const ticket = await tickets.createTicket(req.body)
+        const tickets = await Tickets.get()
+        res.json(tickets)
+    } catch(err) {
+        next(err)
+    }
+})
+
+router.get('/:id', async (req,res,next) => {
+    try {
+        const ticket = await Tickets.getById(req.params.id)
         res.json(ticket)
     } catch(err) {
         next(err)
     }
 })
 
-router.get('/queue', async (req,res,next) => {
+router.get('/:filter/:id', async (req,res,next) => {
     try {
-        res.json(await tickets.getTickets())
+        const tickets = await Tickets.getBy({
+            [req.params.filter]: req.params.id
+        })
+        res.json(tickets)
+    } catch(err) {
+        next(err)
+    }
+})
+
+router.post('/create', async (req,res,next) => {
+    try {
+        const ticket = await Tickets.create({
+            user_id: req.body.user_id,
+	        title: req.body.title,
+	        description: req.body.description,
+	        tried: req.body.tried,
+	        category: req.body.category
+        })
+        res.json(ticket)
+    } catch(err) {
+        next(err)
+    }
+})
+
+router.put('/:id/helper/:helper_id', async (req,res,next) => {
+    const { id, helper_id } = req.params
+    try {
+        const ticket = await Tickets.addHelper(id, helper_id)
+        res.json(ticket)
+    } catch(err) {
+        next(err)
+    }
+})
+
+router.put('/:id/status', async (req,res,next) => {
+    try {
+        const ticket = await Tickets.updateStatus(req.params.id, req.body.status)
+        res.json(ticket)
     } catch(err) {
         next(err)
     }
